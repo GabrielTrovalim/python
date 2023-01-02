e1ec3489168699a347d14f198b1aeda078fa9fb2
Gabriel Trovalim Tavares do Nascimento <gabrieltrovalim@gmail.com>
2023-01-02 03:46:12 -0300

    Exercícios em Python

---
+++ Desafios/mundo01/d002.py
@@ -0,0 +1,7 @@
+print('Gostariamos de saber a sua data de nascimento.\n')
+
+dia = input('Em que dia você nasceu? ')
+mes = input('\nEm que mês você nasceu? ')
+ano = input('\nEm que ano você nasceu? ')
+
+print('\nVocê nasceu em', dia,'de',mes,'de', ano,'. Correto?')
---
+++ Desafios/mundo01/d003.py
@@ -0,0 +1,7 @@
+print('Indique dois números para serem somados\n')
+
+num1 = int (input('Digite o primeiro número: '))
+num2 = int (input('\nDigite o segundo número: '))
+result = (num1+num2)
+
+print('\nA soma é igual a', result)
