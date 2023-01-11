b20ebd998d9979e8d3cdd698e82b740c5c1ba2ae
Gabriel Trovalim Tavares do Nascimento <gabrieltrovalim@gmail.com>
2023-01-11 18:47:55 -0300

    desafios

---
+++ Desafios/mundo01/d016.py
@@ -0,0 +1,6 @@
+import math
+num = float(input('Digite um número: '))
+
+trunca = math.trunc(num)
+
+print('A parte inteira do número {} é igual a {}'.format(num , trunca))
---
+++ Desafios/mundo01/d017.py
@@ -0,0 +1,7 @@
+import emoji
+import math
+
+print(emoji.emojize("Usando pit :silhueta_de_busto:", language='pt'))
+
+catop = float(int('\nDigite o valor do cateto oposto: '))
+catad = float(int('\nDigite o valor do cateto adjacente: '))
