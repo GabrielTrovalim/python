92d40c25c8e92db30d9e1de54fab4e05770b2bcb
Gabriel Trovalim Tavares do Nascimento <gabrieltrovalim@gmail.com>
2023-01-11 19:46:29 -0300

    exercícios

--- Desafios/mundo01/d017.py
+++ Desafios/mundo01/d017.py
@@ -1,7 +1,13 @@
 import emoji
 import math
 
-print(emoji.emojize("Usando pit :silhueta_de_busto:", language='pt'))
+print(emoji.emojize("Usando pit :cérebro:", language='pt'))
 
-catop = float(int('\nDigite o valor do cateto oposto: '))
-catad = float(int('\nDigite o valor do cateto adjacente: '))
+catop = float(input('\nDigite o valor do cateto maior: '))
+catad = float(input('\nDigite o valor do cateto melhor: '))
+
+#A hipotenusa é calculada a partir do teorema de pitagoras, que leva a raiz da soma dos dos quadrados dos catetos.
+
+hip = math.sqrt((catop**2) + (catad**2))
+
+print(emoji.emojize('\nA hipotenusa é igual a: {:.2f} :astonished:'.format(hip), language='alias'))
---
+++ Desafios/mundo01/d019.py
@@ -0,0 +1,7 @@
+import random
+
+
+lista = {'Gabo': 1, 'Gabi': 2, 'Dalila': 3, 'Maria': 4}
+name, id = random.choice(list(lista.items()))
+
+print(name)
