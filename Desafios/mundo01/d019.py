c8c72e157213ef2907ade8baecc62adc3a70c1d9
Gabriel Trovalim Tavares do Nascimento <gabrieltrovalim@gmail.com>
2023-01-12 10:14:04 -0300

    Correções

--- Desafios/mundo01/d019.py
+++ Desafios/mundo01/d019.py
@@ -3,10 +3,10 @@ import random
 print('\nQuem vai apagar o quadro?\n')
 print('-' * 20)
 
-n1 = str(input('\nQuem é o aluno 1? '))
-n2 = str(input('Quem é o aluno 2? '))
-n3 = str(input('Quem é o aluno 3? '))
-n4 = str(input('Quem é o aluno 4? '))
+n1 = str(input('\nPrimeiro aluno: '))
+n2 = str(input('Segundo aluno: '))
+n3 = str(input('Terceiro aluno: '))
+n4 = str(input('Quarto aluno: '))
 
 lista = {n1: 1, n2: 2, n3: 3, n4: 4}
 name, id = random.choice(list(lista.items()))
--- Desafios/mundo01/d020.py
+++ Desafios/mundo01/d020.py
@@ -1,3 +1,12 @@
 import random
 
-#Também não
+n1 = str(input('Primeiro aluno: '))
+n2 = str(input('Segundo aluno: '))
+n3 = str(input('Terceiro aluno: '))
+n4 = str(input('Quarto aluno: '))
+
+lista = [n1, n2, n3, n4]
+random.shuffle(lista) 
+#O comando shuffle embaralha todos os itens contidos na lista e não precisa estar acompanhado de uma variável.
+
+print('\n foi o {}'.format(lista)) # Após o embaralhamento só será necessário printar a lista que foi embaralhada.
