83b21919aaa16fad04e76b600f9fa582ff2245b4
Gabriel Trovalim Tavares do Nascimento <gabrieltrovalim@gmail.com>
2023-01-11 21:12:00 -0300

    Bibliotecas em Python

---
+++ Exercícios/mundo01/Ex005.py
@@ -0,0 +1,5 @@
+import math
+num = int(input('\nDigite um número para saber sua raiz: '))
+raiz = math.sqrt(num)
+
+print('\nA raiz de {} é igual a: {:.2f}'.format(num, raiz))
---
+++ Exercícios/mundo01/Ex006.py
@@ -0,0 +1,2 @@
+import emoji
+print(emoji.emojize("Olá, mundo :medalha_de_ouro:", language='pt'))
