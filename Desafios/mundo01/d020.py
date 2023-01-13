a3a617f1db6cd7df3eecaa08305f56ec9c92dbb6
Gabriel Trovalim Tavares do Nascimento <gabrieltrovalim@gmail.com>
2023-01-12 22:56:28 -0300

    Update d020.py

--- Desafios/mundo01/d020.py
+++ Desafios/mundo01/d020.py
@@ -9,4 +9,4 @@ lista = [n1, n2, n3, n4]
 random.shuffle(lista) 
 #O comando shuffle embaralha todos os itens contidos na lista e não precisa estar acompanhado de uma variável.
 
-print('\n foi o {}'.format(lista)) # Após o embaralhamento só será necessário printar a lista que foi embaralhada.
+print('\nA sequência para a apresentação é {}'.format(lista)) # Após o embaralhamento só será necessário printar a lista que foi embaralhada.
