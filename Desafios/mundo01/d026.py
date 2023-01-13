66c9d5bd0ea7f609ee3b7a5ffc261d408c103af4
Gabriel Trovalim Tavares do Nascimento <gabrieltrovalim@gmail.com>
2023-01-13 02:13:04 -0300

    Exercícios

--- Desafios/mundo01/d026.py
+++ Desafios/mundo01/d026.py
@@ -1,2 +1,7 @@
-fra = str(input('Digite uma frase: '))
-print(fra.count('a'))
+fra = str(input('Digite uma frase: ')).strip()
+up = fra.upper()
+
+print(fra)
+print('A letra A apareceu {} vezes na frase'.format(up.count('A')))
+print('A primeira letra A apareceu na posição {}'.format(up.find('A')+1))
+print('A última letra A apareceu na posição {}'.format(up.rfind('A')+1))
