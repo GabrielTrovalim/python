f86d1a32fccfa8b7ae6d43d9285400513ec75e85
Gabriel Trovalim Tavares do Nascimento <120585461+GabrielTrovalim@users.noreply.github.com>
2023-02-14 06:30:34 -0300

    Mudanças no print do desafio
    Utilizando uma formatação para o print.

--- Desafios/aula01/d003.py
+++ Desafios/aula01/d003.py
@@ -2,5 +2,7 @@ n1 = float(input('Digite um número: '))
 n2 = float(input('Digite um número para ser somado ao primeiro: '))
 # Utilizando o tipo float, para aceitar números de ponto flutuante.
 soma = n1 + n2
+#print('\nA soma entre o número',n1,'e o número',n2,'é igual a:',soma)
 
-print('\nA soma entre o número',n1,'e o número',n2,'é igual a:',soma)
+# Nova versão de print:
+print('\nA soma entre o número {} e o número {} é igual a: {}'.format(n1, n2, soma))
