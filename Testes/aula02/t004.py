6b0aff01cc5c2a917313b89b683fafad9f118bd0
Gabriel Trovalim Tavares do Nascimento <120585461+GabrielTrovalim@users.noreply.github.com>
2023-02-14 05:51:34 -0300

    teste de formatação

---
+++ Testes/aula02/t004.py
@@ -0,0 +1,5 @@
+n1 = float(input('Digite o primeiro número: '))
+n2 = float(input('Digite o segundo número:'))
+soma = n1 + n2
+print('\nA soma entre {} e {} é igual a: {}'.format(n1,n2,soma))
+# Testando a formatação de prints.
