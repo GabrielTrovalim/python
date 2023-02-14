04a95786d3f28b00b5f4a8097af1ff2014f7ecf3
Gabriel Trovalim Tavares do Nascimento <120585461+GabrielTrovalim@users.noreply.github.com>
2023-02-14 06:58:08 -0300

    Adicionando funcionalidades

--- Desafios/aula01/d004.py
+++ Desafios/aula01/d004.py
@@ -1,6 +1,7 @@
 inf = input('Digite algo: ') # Recebendo um input do usuário.
 
 print('')
+print(type(inf))
 if (inf.isnumeric() == True): print('Sua informação é numérica') # isnumeric entre outros sempre retornam true ou false, por isso podemos utilizar um if else.
 else: print('Sua informação não é numérica')
 
@@ -11,4 +12,10 @@ if (inf.isalnum() == True): print('Sua informação é alfanumérica')
 else: print('Sua informação não é alfanumérica')
 
 if (inf.isupper() == True): print('Todas as letras estão em caixa alta')
-else: print('Nem todas as letras estão em caixa alta')
+else: print('As letras não estão totalmente em caixa alta')
+
+if (inf.isspace() == True): print('Só há espaços nessa informação')
+else: print('Há mais do que apenas espaços na sua informação')
+
+if (inf.istitle() == True): print('Sua informação está capitalizada')
+else: print('Sua informação não está capitalizada') # .istitle diz se a string condiz com um título (capitalizada) -> com a primeira letra maiúscula.
