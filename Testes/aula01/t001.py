0792d2c80af3bbc139dc741140d0daa06f6737aa
Gabriel Trovalim Tavares do Nascimento <120585461+GabrielTrovalim@users.noreply.github.com>
2023-02-14 03:55:04 -0300

    Juntando variáveis e texto

---
+++ Testes/aula01/t001.py
@@ -0,0 +1,10 @@
+print('Olá, mundo!') # Exibindo uma mensagem com print('')
+
+print(7+4) # Realizando uma operação e exibindo o resultado com print()
+
+print('7'+'4') # Exibindo uma concatenação de strings
+print('7','4')
+
+print('Olá' + '5') # Observando a diferença entre utilizar um '+' e uma ','
+print('Olá', 5)
+print('Olá','5')
---
+++ Testes/aula01/t002.py
@@ -0,0 +1,9 @@
+nome = 'Gabriel'
+idade = 19
+corcab  = 'Preto' # Cor do cabelo
+
+# Exibindo o resultado:
+
+print('Seu nome é:',nome,'\nVocê tem',idade,'anos\nE a cor do seu cabelo é',corcab)
+# A vírgula já aplica um espaço de maneira prévia ao print.
+# \n faz a quebra de linha
