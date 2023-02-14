f97ce5f6402ff36ad76ef4a130948fe327b7b671
Gabriel Trovalim Tavares do Nascimento <120585461+GabrielTrovalim@users.noreply.github.com>
2023-02-14 04:07:43 -0300

    Variáveis e prints

---
+++ Testes/aula01/t003.py
@@ -0,0 +1,7 @@
+# Recebendo informações do usuário.
+nome = input('Qual é o seu nome? ')
+idade = input('Quantos anos você tem? ')
+peso = input('Qual é o seu peso? ')
+
+print('\nO seu nome é',nome,'\nSeu peso é',peso,'Kg','\nVocê tem',idade,'anos')
+# Utilizando print para mostrar os resultados dos inputs.
