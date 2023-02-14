cb3f0f11617349910974b2ecae8bcf158c1ab8f1
Gabriel Trovalim Tavares do Nascimento <120585461+GabrielTrovalim@users.noreply.github.com>
2023-02-14 04:21:03 -0300

    primeiros desafios

---
+++ Desafios/aula01/d001.py
@@ -0,0 +1,4 @@
+# Desafio 01:
+nome = input('Qual é o seu nome? ')
+
+print('\nOlá,',nome,'! Prazer em te conhecer!')
---
+++ Desafios/aula01/d002.py
@@ -0,0 +1,7 @@
+dia = input('Em que dia você nasceu? ')
+mes = input('Em que mês você nasceu? ')
+ano = int(input('Em que ano você nasceu? '))
+age = 2023 - ano # Faz a contagem da sua idade, considerando que você já tenha feito aniversário.
+
+print('\nVocê nasceu no dia',dia,'do mês de',mes,'no ano de',ano,'.')
+print('Com essas informações, é possível saber que você tem',age,'anos.')
