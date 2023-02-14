0abb619ce98c4c044759a3518a4115c7c2d8dd82
Gabriel Trovalim Tavares do Nascimento <120585461+GabrielTrovalim@users.noreply.github.com>
2023-02-14 08:03:52 -0300

    Formatação modificada

--- Testes/aula03/too6.py
+++ Testes/aula03/too6.py
@@ -2,5 +2,5 @@ print('\nSeu número é par ou ímpar?\n')
 nume = float(input('Digite um número: '))
 
 verifi = nume % 2
-if(verifi == 0): print('O número {} é par!'.format(nume))
-else: print('O número {} é ímpar!'.format(nume))
+if(verifi == 0): print('O número {:.0f} é par!'.format(nume))
+else: print('O número {:.0f} é ímpar!'.format(nume))
