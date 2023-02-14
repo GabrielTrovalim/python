ea589d6643c9e19994c71e21db1527aec3c24018
Gabriel Trovalim Tavares do Nascimento <120585461+GabrielTrovalim@users.noreply.github.com>
2023-02-14 04:31:08 -0300

    aceitando inputs numéricos.

---
+++ Desafios/aula01/d003.py
@@ -0,0 +1,6 @@
+n1 = float(input('Digite um número: '))
+n2 = float(input('Digite um número para ser somado ao primeiro: '))
+# Utilizando o tipo float, para aceitar números de ponto flutuante.
+soma = n1 + n2
+
+print('\nA soma entre o número',n1,'e o número',n2,'é igual a:',soma)
