15ae24d5a6f97616f2399efec1fab3e77fbb9973
Gabriel Trovalim Tavares do Nascimento <120585461+GabrielTrovalim@users.noreply.github.com>
2023-02-15 18:15:43 -0300

    Reajustes simples

--- Desafios/aula03/d007.py
+++ Desafios/aula03/d007.py
@@ -2,6 +2,6 @@ nota1 = float(input('Digite a primeira nota: '))
 nota2 = float(input('Digite a segunda nota: '))
 
 med = (nota1 + nota2)/2 # Média simples.
-print('A sua média é igual a: {:.1f}'.format(med)) # A nota só mostrará duas casas após a virgula.
+print('\nA sua média é igual a: {:.1f}'.format(med)) # A nota só mostrará duas casas após a virgula.
 if (med > 7.0): print('Você está aprovado!')
 else: print('Você está reprovado!')
--- Desafios/aula03/d008.py
+++ Desafios/aula03/d008.py
@@ -3,5 +3,9 @@ met = float(input('Por favor informe a medida em metros: '))
 
 cent = met * 100 # Passando para centimetros.
 mili = met * 1000 # Passando para milímetros.
-
-print('Sua medida tem {} metros.\nEm centimetros tem {} cm.\nEm milímetros tem {} mm.'.format(met, cent, mili))
+dm = met * 10
+dam = met / 10
+hm = met / 100
+km = met / 1000
+# Escala = km hm dam m dm cm mm.
+print('\nA medida de {:.1f}m corresponde a\n{} Km \n{} hm \n{} dam \n{:.0f} dm \n{:.0f} cm \n{:.0f} mm'.format(met,km, hm, dam, dm, cent, mili))
