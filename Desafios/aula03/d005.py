83e90618567b4ffa74010a74b46328d131e2102d
Gabriel Trovalim Tavares do Nascimento <120585461+GabrielTrovalim@users.noreply.github.com>
2023-02-15 19:08:49 -0300

    modificação simples

--- Desafios/aula03/d005.py
+++ Desafios/aula03/d005.py
@@ -3,4 +3,4 @@ n1 = int(input('Digite um número inteiro: '))
 ante = n1 - 1 # Resultado do número antecessor.
 suce = n1 + 1 # Resultado do número sucessor.
 
-print('O número digitiado foi: {}\nSeu sucessor é: {}\nSeu antecessor é: {}'.format(n1,suce,ante))
+print('\nO número digitiado foi: {}\nSeu sucessor é: {}\nSeu antecessor é: {}'.format(n1,suce,ante))
