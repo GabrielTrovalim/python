eb423a971169265db12db4ebf16c9c54248e6c03
Gabriel Trovalim Tavares do Nascimento <120585461+GabrielTrovalim@users.noreply.github.com>
2023-02-22 16:17:29 -0300

    Update d020.py

--- Desafios/aula04/d020.py
+++ Desafios/aula04/d020.py
@@ -1,4 +1,8 @@
 import random # Importando a biblioteca random.
-lista = ['Gabriel', 'João', 'Zézão', 'isabela', 'Joana', 'zuleide'] # Alunos -> de 0 a 5
-escolha = random.Random
-print('Ordem = {}'.format(lista[escolha]))
+nome1 = str(input('Primeiro aluno: '))
+nome2 = str(input('Segundo aluno: '))
+nome3 = str(input('Terceiro aluno: '))
+nome4 = str(input('Quarto aluno: '))
+lista = [nome1, nome2, nome3, nome4] # Criando a lista com os inputs.
+random.shuffle(lista) # Embaralhando os nomes na lista.
+print('A ordem de apresentação é a seguinte:\n{}'.format(lista))
