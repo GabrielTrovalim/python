2fe38619f8d78ebad07ac45e64b309dee44baa56
Gabriel Trovalim Tavares do Nascimento <120585461+GabrielTrovalim@users.noreply.github.com>
2023-02-22 19:40:20 -0300

    Corrigindo um bug

--- Desafios/aula05/d022.py
+++ Desafios/aula05/d022.py
@@ -1,6 +1,9 @@
 num = int(input('Digite um número entre 0 e 9999: '))
 if (num > 9999): print('Você digitou um número fora do esperado.')
-else: 
-    snum = str(num)
-    print('\nO número digitado foi {}'.format(num))
-    print('\nUnidade: {}\nDezena: {}\nCentena: {}\nMilhar: {}'.format(snum[3], snum[2], snum[1], snum[0]))
+else:
+    uni = num // 1 % 10
+    dez = num // 10 % 10
+    cent = num // 100 % 10
+    mili = num // 1000 % 10 
+    print('O número digitado foi: {}'.format(num))
+    print('\nMilhar: {}\nCentena: {}\nDezena: {}\nUnidade: {}'.format(mili, cent, dez, uni))
