81d42b596d049f705a792088d853fc74ab969570
Gabriel Trovalim Tavares do Nascimento <120585461+GabrielTrovalim@users.noreply.github.com>
2023-02-22 20:25:37 -0300

    Verificador de nome

---
+++ Desafios/aula05/d026.py
@@ -0,0 +1,3 @@
+nome = str(input('Digite o seu nome completo: ')).strip().title()
+sepa = nome.split()
+print('\nÉ um prazer te conhecer, {}.\nSeu primeiro nome é: {}\nSeu último nome é: {}'.format(nome, sepa[0], sepa[len(sepa)- 1]))
