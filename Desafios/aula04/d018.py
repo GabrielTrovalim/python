f35fa75e7df0a17bb01f607385907736b99f70d8
Gabriel Trovalim Tavares do Nascimento <120585461+GabrielTrovalim@users.noreply.github.com>
2023-02-22 15:42:04 -0300

    Update d018.py

--- Desafios/aula04/d018.py
+++ Desafios/aula04/d018.py
@@ -3,4 +3,4 @@ ang = float(input('Digite o valor de um angulo: '))
 sen = math.sin(math.radians(ang)) # Calculando o seno.
 cose = math.cos(math.radians(ang)) # Calculando o cosseno.
 tg = math.tan(math.radians(ang)) # Calculando a tangente.
-print('Para o angulo de {}° temos: \nSeno = {:.2f} \nCosseno = {:.2f} \nTangente = {:.2f}'.format(ang, sen, cose, tg))
+print('Para o ângulo de {}° temos: \nSeno = {:.2f} \nCosseno = {:.2f} \nTangente = {:.2f}'.format(ang, sen, cose, tg))
