3d75c58108e9178dc203194915b54d301c2df925
Gabriel Trovalim Tavares do Nascimento <120585461+GabrielTrovalim@users.noreply.github.com>
2023-02-22 14:10:14 -0300

    corrigindo medidas

--- Desafios/aula04/d018.py
+++ Desafios/aula04/d018.py
@@ -1,6 +1,6 @@
 import math # Importando a biblioteca de matemática.
-ang = int(input('Digite o valor de um angulo: '))
-sen = math.sin(ang) # Calculando o seno.
-cose = math.cos(ang) # Calculando o cosseno.
-tg = math.tan(ang) # Calculando a tangente.
-print('Para o angulo de {}° temos: \nSeno = {:.3f} \nCosseno = {:.3f} \nTangente = {:.3f}'.format(ang, sen, cose, tg))
+ang = float(input('Digite o valor de um angulo: '))
+sen = math.sin(math.radians(ang)) # Calculando o seno.
+cose = math.cos(math.radians(ang)) # Calculando o cosseno.
+tg = math.tan(math.radians(ang)) # Calculando a tangente.
+print('Para o angulo de {}° temos: \nSeno = {:.2f} \nCosseno = {:.2f} \nTangente = {:.2f}'.format(ang, sen, cose, tg))
