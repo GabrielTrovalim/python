32d7f48f050107985ecacca7e444682a6fcd03f4
Gabriel Trovalim Tavares do Nascimento <120585461+GabrielTrovalim@users.noreply.github.com>
2023-02-22 20:15:40 -0300

    Atualização

--- Desafios/aula05/d025.py
+++ Desafios/aula05/d025.py
@@ -1,5 +1,6 @@
-frase = str(input('Escreva uma frase: ')).strip().capitalize()
-big = frase.upper()
-proc = big.find('A')
-con = big.count('A')
-print('A frase\n{}\nTem {} aparições da letra "A"\nA primeira aparição da letra "A" é na posição {}'.format(frase, con, proc))
+frase = str(input('Escreva uma frase: ')).strip().capitalize().upper()
+most = frase.title()
+proc = frase.find('A') + 1
+procf = frase.rfind('A') + 1
+con = frase.count('A')
+print('\nA frase: {}\nTem {} aparições da letra "A"\nA primeira aparição da letra "A" é na posição {}\nA última aparição da letra "A" foi na posição {}'.format(most, con, proc, procf))
