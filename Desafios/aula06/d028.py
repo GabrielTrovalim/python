2413089b9a5a6974701f76da7f44d16eaf526d4a
Gabriel Trovalim Tavares do Nascimento <120585461+GabrielTrovalim@users.noreply.github.com>
2023-02-23 19:18:56 -0300

    utilizando a biblioteca time

--- Desafios/aula06/d028.py
+++ Desafios/aula06/d028.py
@@ -1,9 +1,12 @@
 import random
-u_num = int(input('Digite um número de 0 a 5: '))
-if(u_num > 5 or u_num < 0):
+from time import sleep # Importando a função sleep que faz o computador esperar.
+u_num = int(input('\nDigite um número de 0 a 5: '))
+if(u_num > 5 or u_num < 0): # Valindando a entrada de dados do usuário.
     print('[ERRO] Entrada de dados invalida.')
 else:
-    num = random.randint(0 ,5)
+    print('\nProcessando...')
+    sleep(2) # esperando dois segundos.
+    num = random.randint(0 ,5) # Faz o computador sortear um número.
     if(u_num == num):
         print('\nO número sorteado foi: {}'.format(num))
         print('Você acertou o número sorteado!')
