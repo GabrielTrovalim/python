043b4c99003b0ce4aa1e0aef4bcd8a98e7ba34e0
Gabriel Trovalim Tavares do Nascimento <120585461+GabrielTrovalim@users.noreply.github.com>
2023-02-23 20:45:50 -0300

    Update utilizando a data atual

--- Desafios/aula06/d032.py
+++ Desafios/aula06/d032.py
@@ -1,17 +1,35 @@
-print('\nVerifique se um ano qualquer é bissexto\n')
-ano = int(input('Digite um ano de sua preferência: '))
-verifi = ano % 4
-if (ano < 0): 
-    print('Ano invalido, por favor tente novamente...')
-else:
-    if(ano > 2023):
-        if(verifi == 0):
-            print('O ano de {} será bissexto!'.format(ano))
-        else: 
-            print('O ano de {} não será bissexto...'.format(ano))
+from datetime import date
+from time import sleep
+print('\nVerifique se um ano qualquer é bissexto')
+ano = int(input('\nDigite o ano que deseja analisar\nCaso zero seja digitado, o ano atual será levado em consideração.\nAno: '))
+hoje = date.today().year
+if (ano == 0):
+    ano = date.today().year
+    print('\nVocê escolheu analisar o ano atual')
+    print('Processando...')
+    sleep(2)
+    if (ano % 4 == 0 and ano % 10 != 0 or ano % 400 == 0):
+        print('O ano atual ({}) é bissexto.'.format(ano))
     else:
-        if(verifi == 0):
-            print('O ano de {} foi bissexto!'.format(ano))
+        print('O ano atual ({}) não é bissexto.'.format(ano))
+else:
+    if (ano < hoje):
+        print('Processando...')
+        sleep(2)
+        if (ano % 4 == 0 and ano % 10 != 0 or ano % 400 == 0):
+            print('O ano de {} foi bissexto.'.format(ano))
         else:
-            print('O ano de {} não foi bissexto...'.format(ano))
-print('Fim da verificação.')
+            print('O ano de {} não foi bissexto.'.format(ano))
+    else:
+        print('processando...')
+        sleep(2)
+        if (ano % 4 == 0 and ano % 10 != 0 or ano % 400 == 0):
+            print('O ano de {} será bissexto.'.format(ano))
+        else:
+            print('O ano de {} não será bissexto.'.format(ano))
+
+
+'''if(ano % 4 == 0 and ano % 10 != 0 or ano % 400 == 0):
+    print('O ano de {} é bissexto.'.format(ano))
+else:
+    print('O ano de {} não é bissexto.'.format(ano))'''
