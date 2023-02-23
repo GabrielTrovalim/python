2c1f7b64ebb91b45eee9034d994f4d1cb70cd734
Gabriel Trovalim Tavares do Nascimento <120585461+GabrielTrovalim@users.noreply.github.com>
2023-02-23 20:47:22 -0300

    Update d032.py

--- Desafios/aula06/d032.py
+++ Desafios/aula06/d032.py
@@ -3,7 +3,7 @@ from time import sleep
 print('\nVerifique se um ano qualquer é bissexto')
 ano = int(input('\nDigite o ano que deseja analisar\nCaso zero seja digitado, o ano atual será levado em consideração.\nAno: '))
 hoje = date.today().year
-if (ano == 0):
+if (ano == 0): # Caso o ano atual seja analisado:
     ano = date.today().year
     print('\nVocê escolheu analisar o ano atual')
     print('Processando...')
@@ -12,7 +12,7 @@ if (ano == 0):
         print('O ano atual ({}) é bissexto.'.format(ano))
     else:
         print('O ano atual ({}) não é bissexto.'.format(ano))
-else:
+else: # Caso outro ano seja analisado, no futuro ou passado:
     if (ano < hoje):
         print('Processando...')
         sleep(2)
@@ -27,9 +27,3 @@ else:
             print('O ano de {} será bissexto.'.format(ano))
         else:
             print('O ano de {} não será bissexto.'.format(ano))
-
-
-'''if(ano % 4 == 0 and ano % 10 != 0 or ano % 400 == 0):
-    print('O ano de {} é bissexto.'.format(ano))
-else:
-    print('O ano de {} não é bissexto.'.format(ano))'''
