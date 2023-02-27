8f5d5c95fc7952f3fd3fec55fefe03c2efa53594
Gabriel Trovalim Tavares do Nascimento <120585461+GabrielTrovalim@users.noreply.github.com>
2023-02-27 19:28:46 -0300

    Versão final pronta

--- Desafios/aula08/d036.py
+++ Desafios/aula08/d036.py
@@ -1,12 +1,17 @@
 from time import sleep
-nome = str(input('Insira o nome do cliente: '))
-val_casa = float(input('\nQual é o valor da casa a ser financiada? '))
-salario = float(input('Qual o salário do cliente? '))
-sleep (2)
-print('\nOkay... Informações guardadas.')
-sleep (2)
-time = int(input('\nEm quantos anos o cliente pretende pagar a divida? '))
-sleep (2)
+nome = str(input('\nDigite o nome do cliente: ')).strip().title()
+v_casa = float(input('Diga o valor da casa que o cliente pretende comprar: \nR$'))
+salario = float(input('Por favor, informe o salário do cliente: \nR$'))
+q_anos = int(input('Quantidade de anos para a divida ser quitada: '))
+# contas:
+v_parcela = v_casa / (q_anos * 12)
+porcent = salario * 0.3 # 30% do salário do cliente.
+sleep(1)
+print('\nInformações guardadas')
 print('Calculando...')
-sleep (2)
-print('fim do teste.')
+sleep(2)
+if (v_parcela > porcent):
+    print('\nInfelizmente o emprestimo foi negado...\nO valor de R${:.2f} mensais ultrapassam 30% do salário do cliente.'.format(v_parcela))
+else:
+    print('\nO seu pedido de emprestimo foi aceito!\nValor mensal a pagar: R${:.2f}\nQuantidade de parcelas: {}'.format(v_parcela, q_anos * 12))
+print('\nTenha um ótimo dia, {}!'.format(nome))
