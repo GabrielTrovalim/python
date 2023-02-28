925199467ec450bc4525c15d660634f46b1ed28a
Gabriel Trovalim Tavares do Nascimento <120585461+GabrielTrovalim@users.noreply.github.com>
2023-02-27 23:20:33 -0300

    Update d039.py

--- Desafios/aula08/d039.py
+++ Desafios/aula08/d039.py
@@ -1,10 +1,17 @@
 from datetime import date
+from time import sleep
 ano_atual = date.today().year # Pegando o ano atual do S.O
 ano_nasc = int(input('Em que ano você nasceu? '))
-age = ano_atual - ano_nasc
-if (age > 18): 
-    print('Você provavelmente já se apresentou ao exercíto.')
+age = ano_atual - ano_nasc # Idade do usuário.
+dezoito = age - 18
+data = ano_atual - dezoito
+print('\nVerificando...')
+sleep(2)
+if (age > 18):
+    print('\nCom {} anos, você já passou pelo alistamento militar.'.format(age))
+    print('Você se apresentou ao exercito a {} anos, em {}.'.format(dezoito, data))
 elif (age < 18):
-    print('Você ainda precisa se apresentar ao exercíto.')
-else:
-    print('Esse ano você se apresentará ao exercíto.')
+    print('Com {} anos, você ainda não passou pelo alistamento militar.')
+    
+    
+print(data)
