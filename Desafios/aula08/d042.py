f6d59d2ca75da5c62e938912d83ba19b1cc8d213
Gabriel Trovalim Tavares do Nascimento <120585461+GabrielTrovalim@users.noreply.github.com>
2023-02-28 17:24:12 -0300

    tipos de triângulo.

---
+++ Desafios/aula08/d042.py
@@ -0,0 +1,17 @@
+print('\nDescreva um triângulo:\n')
+seg1 = float(input('Primeiro segmento: '))
+seg2 = float(input('Segundo segmento: '))
+seg3 = float(input('Terceiro segmento: '))
+if (seg1 <= 0 or seg2 <= 0 or seg3 <= 0):
+    print('\n[ERRO] Entrada de dados invalida.\nReinicie para tentar novamente.')
+else:
+    if (seg1 < seg2 + seg3 and seg2 < seg1 + seg3 and seg3 < seg2 + seg1):
+        print('\nOs segmentos descritos são capazes de formar um triângulo!')
+        if (seg1 == seg2 and seg1 == seg3 and seg3 == seg2):
+            print('O triângulo formado pelos segmentos é do tipo: Equilátero.')
+        elif (seg1 == seg2 or seg1 == seg3 or seg2 == seg3):
+            print('O triângulo formado pelos segmentos é do tipo: Isósceles.')
+        else: 
+            print('Todos os lados do triângulo são diferentes.')
+    else:
+        print('\nOs segmentos descritos \033[0;31mnão\033[m são capazes de formar um triângulo...')
