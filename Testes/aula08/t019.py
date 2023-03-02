16b6f7b499a312411bcacb819e9040276f075159
Gabriel Trovalim Tavares do Nascimento <120585461+GabrielTrovalim@users.noreply.github.com>
2023-03-02 17:03:51 -0300

    Update t019.py

--- Testes/aula08/t019.py
+++ Testes/aula08/t019.py
@@ -1,5 +1,7 @@
+from time import sleep
 print('-=' * 20)# faz 40 sinais.
 print('{:^40}'.format('Calculadora'))
 print('=-' * 20)
 pt_1 = float(input('Digite o primeiro número: '))
 # operações simples possíveis.
+print('\nEscolha a operação a se fazer\n[+] Somar\n[-] Subtrair\n[X] Multiplicar\n[/] Dividir')
