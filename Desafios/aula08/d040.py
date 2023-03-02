752ba6f63e92c1495ad901244d2b3b667e71b04f
Gabriel Trovalim Tavares do Nascimento <120585461+GabrielTrovalim@users.noreply.github.com>
2023-03-01 21:43:46 -0300

    cores

--- Desafios/aula08/d040.py
+++ Desafios/aula08/d040.py
@@ -14,9 +14,9 @@ elif (nota2 > 10 or nota2 < 0):
     print('[ERRO] Entrada de dados invalida.\nReinicie o programa para tentar novamente.')
 else:
     if (med >= 7):
-        print('\nMédia do aluno: {}\nPrimeira nota: {}\nSegunda nota: {}\nSituação: Aprovado!'.format(med, nota1, nota2))
+        print('\nMédia do aluno: {}\nPrimeira nota: {}\nSegunda nota: {}\nSituação: \033[0;32mAprovado!\033[m'.format(med, nota1, nota2))
     elif (med < 5):
-        print('\nMédia do aluno: {}\nPrimeira nota: {}\nSegunda nota: {}\nSituação: Reprovado, por média insuficiente.'.format(med, nota1, nota2))
+        print('\nMédia do aluno: {}\nPrimeira nota: {}\nSegunda nota: {}\nSituação: \033[0;31mReprovado\033[m, por média insuficiente.'.format(med, nota1, nota2))
     else:
-        print('\nMédia do aluno: {}\nPrimeira nota: {}\nSegunda nota: {}\nSituação: Em recuperação.'.format(med, nota1, nota2))
+        print('\nMédia do aluno: {}\nPrimeira nota: {}\nSegunda nota: {}\nSituação: Em \033[0;33mrecuperação\033[m.'.format(med, nota1, nota2))
 print('\nFim da geração de boletim.\nTenha um bom dia!')
