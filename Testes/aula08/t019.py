0ca5856c28a337d3cdbb05e8ef6151477f30e0c6
Gabriel Trovalim Tavares do Nascimento <120585461+GabrielTrovalim@users.noreply.github.com>
2023-03-02 17:10:38 -0300

    Update t019.py

--- Testes/aula08/t019.py
+++ Testes/aula08/t019.py
@@ -7,3 +7,12 @@ pt_1 = float(input('Digite o primeiro número: '))
 print('\nEscolha a operação a se fazer\n[+] Somar\n[-] Subtrair\n[X] Multiplicar\n[/] Dividir\n')
 sinal = str(input('Sua opção: ')) # Guarda a operação escolhida.
 pt_2 = float(input('\nDigite o segundo número: '))
+print('\nCalculando o seu resultado...')
+sleep(2)
+if (sinal == '+'):
+    resu_soma = pt_1 + pt_2
+    print('\nResultado:\n {:.1f} + {:.1f} = {:.1f}\n'.format(pt_1, pt_2, resu_soma))
+    sleep(1)
+    print('\nObrigado por testar, tenha um bom dia!\n')
+else:
+    print('oi')
