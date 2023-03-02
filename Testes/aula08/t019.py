03087d40019eb1e1e0705f634fb9486cf06f5588
Gabriel Trovalim Tavares do Nascimento <120585461+GabrielTrovalim@users.noreply.github.com>
2023-03-02 17:49:15 -0300

    Update t019.py

--- Testes/aula08/t019.py
+++ Testes/aula08/t019.py
@@ -5,7 +5,7 @@ print('=-' * 20)
 pt_1 = float(input('Digite o primeiro número: '))
 # operações simples possíveis.
 print('\nEscolha a operação a se fazer\n[+] Somar\n[-] Subtrair\n[X] Multiplicar\n[/] Dividir\n')
-sinal = str(input('Sua opção: ')) # Guarda a operação escolhida.
+sinal = str(input('Sua opção: ')).upper() # Guarda a operação escolhida.
 pt_2 = float(input('\nDigite o segundo número: '))
 print('\nCalculando o seu resultado...')
 sleep(2)
@@ -19,3 +19,17 @@ elif (sinal == '-'):
     print('\nResultado:\n {:.1f} - {:.1f} = {:.1f}'.format(pt_1, pt_2, resu_sub))
     sleep(1)
     print('\nObrigado por testar, tenha um bom dia!\n')
+elif (sinal == 'X'):
+    resu_mult = pt_1 * pt_2
+    print('\nResultado:\n {:.1f} X {:.1f} = {:.1f}'.format(pt_1, pt_2, resu_mult))
+    sleep(2)
+    print('\nObrigado por testar, tenha um bom dia!\n')
+elif (sinal == '/'):
+    resu_div = pt_1 / pt_2
+    print('\nResultado:\n {:.1f} / {:.1f} = {:.1f}'.format(pt_1, pt_2, resu_div))
+    sleep(2)
+    print('\nObrigado por testar, tenha um ótimo dia!\n')
+else:
+    print('\n[ERRO] entrada de dados invalida. Tente novamente!\n')
+    sleep(2)
+    print('FIM')
