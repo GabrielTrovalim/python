25c0b1e1a1b041ecbae16b3ca89c87eb7d749916
Gabriel Trovalim Tavares do Nascimento <120585461+GabrielTrovalim@users.noreply.github.com>
2023-03-03 15:50:03 -0300

    contagem

---
+++ Desafios/aula09/d046.py
@@ -0,0 +1,10 @@
+from time import sleep
+from datetime import date
+atual = date.today().year
+# contagem regressiva:
+print('\nVai começar a contagem regressiva.\n')
+sleep(2) 
+for c in range(10, 0, -1):
+    print(c)
+    sleep(1)
+print('\nFeliz ano novo!!\nFeliz {}!\n'.format(atual + 1))
