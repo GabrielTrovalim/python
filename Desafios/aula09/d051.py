9a067087c0c919e0bc87e4d608d1c6a8135b0c1f
Gabriel Trovalim Tavares do Nascimento <120585461+GabrielTrovalim@users.noreply.github.com>
2023-03-03 20:44:34 -0300

    novas tentativas

--- Desafios/aula09/d051.py
+++ Desafios/aula09/d051.py
@@ -1,4 +1,14 @@
-pt_pa = int(input('\nDigite o primeiro termo da p.a: '))
-ra_pa = int(input('Digite a razão da p.a: '))
-for c in range(pt_pa, pt_pa + 10):
-    print((c - 1) * ra_pa)
+p_pa = int(input('\nDigite o primeiro termo da p.a: ')) # Primeiro termo da p.a
+r_pa = int(input('Digite a razão da p.a: ')) # Razão da p.a
+rd = r_pa * 10
+pd = (p_pa + 10) # quando a razão é igual a 1
+dr = (r_pa * 10) + 16
+if (r_pa > p_pa):
+    for c in range (p_pa,rd,r_pa): # se r_pa for maior que p_pa
+        print(c)
+elif (r_pa == 1):
+    for c in range (p_pa,pd,r_pa): # se a razão for igual a um
+        print(c)
+elif (r_pa < p_pa):
+    for c in range (p_pa, dr, r_pa):
+        print(c)
---
+++ Testes/aula09/t020.py
@@ -0,0 +1,4 @@
+for c in range(87,145,13): # r_pa < p_pa -> (r_pa * 10) + 10
+    print(c)
+
+# funciona mas se r_pa for p_pa - 10 para
