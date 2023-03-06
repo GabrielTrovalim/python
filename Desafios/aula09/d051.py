206370773b7ad0440359ddf9fbd1ac5cdec8aef9
Gabriel Trovalim Tavares do Nascimento <120585461+GabrielTrovalim@users.noreply.github.com>
2023-03-06 16:18:35 -0300

    d051
    Finalização do desafio 51

--- Desafios/aula09/d051.py
+++ Desafios/aula09/d051.py
@@ -1,14 +1,7 @@
-p_pa = int(input('\nDigite o primeiro termo da p.a: ')) # Primeiro termo da p.a
-r_pa = int(input('Digite a razão da p.a: ')) # Razão da p.a
-rd = r_pa * 10
-pd = (p_pa + 10) # quando a razão é igual a 1
-dr = (r_pa * 10) + 16
-if (r_pa > p_pa):
-    for c in range (p_pa,rd,r_pa): # se r_pa for maior que p_pa
-        print(c)
-elif (r_pa == 1):
-    for c in range (p_pa,pd,r_pa): # se a razão for igual a um
-        print(c)
-elif (r_pa < p_pa):
-    for c in range (p_pa, dr, r_pa):
-        print(c)
+# Primeiro elemento da p.a -> de onde parte o nosso programa.
+# Razão da p.a -> passo da contagem
+num_pa = int(input('\nDigite o primeiro termo: '))
+ra = int(input('Digite a razão da p.a: '))
+calc = (ra * 10) + num_pa
+for c in range(num_pa,calc,ra):
+    print(c)
---
+++ Desafios/aula09/d052.py
@@ -0,0 +1,7 @@
+# 1° - num_pa * 2 + ra entrega o limite.
+# 2° - ra * limite.
+for c in range(537, 537 + 40,4):
+    print(c) # teste 01
+# 537 = num_pa -> de onde parte
+# 537 + 40 = calc -> até onde vai
+# 4 = ra -> passo da contagem
