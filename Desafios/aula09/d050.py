7045485b3284f93987cb9b2e351d09c139a140b3
Gabriel Trovalim Tavares do Nascimento <120585461+GabrielTrovalim@users.noreply.github.com>
2023-03-06 23:19:49 -0300

    Update d050.py

--- Desafios/aula09/d050.py
+++ Desafios/aula09/d050.py
@@ -1,6 +1,6 @@
 soma = 0
-for c in range(0,6):
-    num = int(input('Digite um valor inteiro: '))
+for c in range(1,7):
+    num = int(input('Digite o {}° valor: '.format(c)))
     if (num % 2 == 0): # Verificando se o número é par.
         soma = soma + num
-print('\nO somatorio dos números pares digitados é: {}'.format(soma))
+print('\nO somatório dos números pares digitados é: {}'.format(soma))
