3620ad2bc64a6510734e990630525f7fdd546d61
Gabriel Trovalim Tavares do Nascimento <120585461+GabrielTrovalim@users.noreply.github.com>
2023-03-06 23:46:17 -0300

    Update d051.py

--- Desafios/aula09/d051.py
+++ Desafios/aula09/d051.py
@@ -3,5 +3,9 @@
 num_pa = int(input('\nDigite o primeiro termo: '))
 ra = int(input('Digite a razão da p.a: '))
 calc = (ra * 10) + num_pa
+cont = 0 # contador.
+print('')
 for c in range(num_pa,calc,ra):
-    print(c)
+    cont += 1
+    #print(c)
+    print('O {:2}° da P.A é: {}'.format(cont, c))
