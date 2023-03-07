1efba2e15a3d220e37f1115f68867d57e9481632
Gabriel Trovalim Tavares do Nascimento <120585461+GabrielTrovalim@users.noreply.github.com>
2023-03-06 22:19:05 -0300

    Update d046.py

--- Desafios/aula09/d046.py
+++ Desafios/aula09/d046.py
@@ -4,7 +4,7 @@ atual = date.today().year
 # contagem regressiva:
 print('\nVai começar a contagem regressiva.\n')
 sleep(2) 
-for c in range(10, 0, -1):
+for c in range(10, -1, -1):
     print(c)
     sleep(1)
 print('\nFeliz ano novo!!\nFeliz {}!\n'.format(atual + 1))
