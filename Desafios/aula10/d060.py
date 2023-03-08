3c4a118cb90ac29dcc4137af3949697173396f0e
Gabriel Trovalim Tavares do Nascimento <120585461+GabrielTrovalim@users.noreply.github.com>
2023-03-08 20:57:28 -0300

    Create d060.py

---
+++ Desafios/aula10/d060.py
@@ -0,0 +1,9 @@
+cont = 1 # contador inicial.
+num = int(input('Digite um número inteiro: '))
+cont += num # contador inicial recebe o valor inserido.
+fat = 1 # contador fatorial.
+while cont > 1:
+    cont -= 1 # contagem regressiva.
+    fat *= cont # multiplicação da contagem.
+    print(cont, end=' ')
+print('\nResultado fatorial do número digitado: {}\n'.format(fat))
