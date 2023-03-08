28dd954033f6dee0cebab3442443540d2ee4d9e3
Gabriel Trovalim Tavares do Nascimento <120585461+GabrielTrovalim@users.noreply.github.com>
2023-03-08 18:56:49 -0300

    Update d058.py

--- Desafios/aula10/d058.py
+++ Desafios/aula10/d058.py
@@ -1,11 +1,22 @@
 from random import randint
-num_pc = randint(0,10)
-print(num_pc)
-num = int(input('Tente adivinhar o número: '))
-if num == num_pc:
-    print('\nParabéns você ganhou!')
-else:
-    while num != num_pc:
-        print('\nOps, parece que você errou...')
-        num = int(input('Tente novamente: '))
-    print('\nParabéns você ganhou!')
+from time import sleep
+print('=-' * 30)
+print('{:^60}'.format('Jogo do sorteio!'))
+print('-=' * 30)
+jogo = 'S'
+while jogo != 'N':
+    num_pc = randint(0,10)
+    print('\nO computador está sorteando um número...')
+    sleep(2)
+    print('\nPronto.\nNúmero sorteado! {}'.format(num_pc))
+    sleep(1)
+    num = int(input('\nTente adivinhar o número entre 1 e 10\nSua jogada: '))
+    if num == num_pc:
+        print('\nParabéns você ganhou!')
+    else:
+        while num != num_pc:
+            print('\nOps, parece que você errou...')
+            num = int(input('Tente novamente: '))
+        print('\nParabéns você ganhou!')
+    jogo = str(input('\nVocê gostaria de jogar novamente? [S/N]: ')).strip().upper()
+print('\nMuito obrigado por ter jogado!\n')
